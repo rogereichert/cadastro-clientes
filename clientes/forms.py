8c4f5c144d792bc7d9c5396c1cd97b34be2b37ab
Roger <rogereichert@gmail.com>
2026-03-03 13:51:41 -0300

    feat(clientes): add ClienteForm with documento normalization

---
+++ clientes/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Cliente
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        fields = [
+            "nome",
+            "tipo_documento",
+            "documento",
+            "email",
+            "telefone",
+            "cep",
+            "endereco",
+            "numero",
+            "complemento",
+            "bairro",
+            "cidade",
+            "estado",
+        ]
+
+    def clean_documento(self):
+        doc = (self.cleaned_data.get("documento") or "").strip()
+        doc = "".join(ch for ch in doc if ch.isdigit())
+        return doc
+    
